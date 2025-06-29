2af86a55cc16035ed881e8bef0ffca596f089375
VIJAY J <167644890+thevjay@users.noreply.github.com>
2025-06-29 08:41:12 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+exec > /var/log/user-data.log 2>&1
+
+# Update the ubuntu machine
+sudo apt update -y
+
+# Install nodejs in the machine
+sudo apt install nodejs -y
+
+# Install npm in the machine
+sudo apt install npm -y
+
+# Clone the repository
+git clone https://github.com/singhsanket143/AWS-Demo-Project.git /home/ubuntu/project
+
+# cd in the folder
+cd /home/ubuntu/project
+
+# install packages
+sudo npm install
+
+# turn up the server
+sudo npx pm2 start index.js
